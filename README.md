e801feb984b7f279c8e5cde3ce3a912a9cf65309
b_d <brian@tf83.com>
2019-03-29 11:27:18 -0400

    feat: Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+![Travis](https://img.shields.io/travis/brianjd/gatsby-plugin-disable-react-devtools/master.svg)
+# gatsby-plugin-disable-react-devtools
+
+## Description
+
+A plugin to disable React Dev Tools
---
+++ src/__tests__/gatsby-ssr.js
@@ -0,0 +1,10 @@
+const { onRenderBody } = require(`../gatsby-ssr`)
+
+define('gatsby-plugin-disable-react-devtools', () => {
+  it('adds script if in production', () => {
+
+  });
+  it('does not add script if not in production', () => {
+
+  });
+});
